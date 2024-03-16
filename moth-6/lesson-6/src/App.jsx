369b7a88f344dbcc96800df5800e55d1bfca0497
Nabiyevich21 <mmedia10020@gmail.com>
2024-03-17 02:59:40 +0500

    added page to page

---
+++ moth-6/lesson-6/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+export default class App extends Component {
+    render() {
+        return (
+            <>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/about"}>about</Link>
+                <Link to={"/contact"}>contact</Link>
+                <Routes>
+                    <Route path="/" element={<h1>Home Page</h1>} />
+                    <Route path="/about" element={<h1>about Page</h1>} />
+                    <Route path="/contact" element={<h1>contact Page</h1>} />
+                </Routes>
+            </>
+        );
+    }
+}
